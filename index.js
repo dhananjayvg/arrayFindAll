56ccffb2c61fe0a6576df14d3a46bf854235ea26
Dhananjay G <34699640+dhananjayvg@users.noreply.github.com>
2019-10-20 19:50:28 +0530

    added missing return statement

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function findAllOccurances(ar, el) {
         i = ar.indexOf(el, i+1);
         if(i != -1) { result.push(i);}
     } while(i != -1);
+    return result;
 }
 
 const ar = [1, 3, 4, 2, 2, 5, 1, 2, 5, 6, 3, 2];
