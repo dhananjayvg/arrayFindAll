4634ff0adb566eab0d569ebad117f22867064fb8
Dhananjay G <34699640+dhananjayvg@users.noreply.github.com>
2019-10-20 19:44:21 +0530

    find all occurances of an element in array method
    find all occurances of an element in array method

---
+++ index.js
@@ -0,0 +1,12 @@
+function findAllOccurances(ar, el) {
+    let i=-1;
+    const result = [];
+    do {
+        i = ar.indexOf(el, i+1);
+        if(i != -1) { result.push(i);}
+    } while(i != -1);
+}
+
+const ar = [1, 3, 4, 2, 2, 5, 1, 2, 5, 6, 3, 2];
+const el = 2;
+console.log(findAllOccurances(ar,el));
